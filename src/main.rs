a78b666c9852f980e7cab778dcc707b1ceff421d
Lucas-BRT <93885104+Lucas-BRT@users.noreply.github.com>
2023-09-26 08:36:30 -0300

    add: metadata for publish

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,14 @@
 ///! kenku_control
 ///!
 ///! A library for manage your Kenku FM using remote acess.
+
 mod lib;
 use lib::Controller;
 
-use std::thread;
-use std::time::Duration;
 
-extern crate kenku_control;
-
-#[tokio::main]
-async fn main() {
+fn main() {
     let ip = "127.0.0.1".to_string();
     let port = "3333".to_string();
 
     let controller = Controller::new(ip, port);
-
-    let playlist = controller.get_soundboard().await.unwrap();
-    let tracks = playlist.sounds;
-
-    for track in tracks {
-        track.play(&controller).await;
-    }
-}
+} 
