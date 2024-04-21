ffbb0ca1b1fd368768f64a6ac4ae4d47fce4bd0d
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-04-21 11:24:00 -0300

    TEST: basic structure for start testing

---
+++ tests/basic_commands_test.rs
@@ -0,0 +1,10 @@
+use kenku_control::*;
+
+const DEFAULT_IP_ADDRESS: &str = "127.0.0.1";
+const DEFAULT_PORT: u16 = 3333;
+
+#[tokio::test]
+async fn kenku_remote_is_online() {
+    let server_state = check_kenku_server_state(DEFAULT_IP_ADDRESS, DEFAULT_PORT).await;
+    assert_eq!(server_state, KenkuState::Online);
+}
