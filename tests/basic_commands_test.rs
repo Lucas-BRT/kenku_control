d640dbe34f0023dc54adb3bfdd6b34079e2b4854
lucasbrt-desktop <lucasbritodepaula.t@gmail.com>
2024-04-21 13:45:04 -0300

    TEST: check if kenku remote is online

--- tests/basic_commands_test.rs
+++ tests/basic_commands_test.rs
@@ -5,6 +5,6 @@ const DEFAULT_PORT: u16 = 3333;
 
 #[tokio::test]
 async fn kenku_remote_is_online() {
-    let server_state = check_kenku_server_state(DEFAULT_IP_ADDRESS, DEFAULT_PORT).await;
+    let server_state = check_kenku_server_state(DEFAULT_IP_ADDRESS, DEFAULT_PORT, 10).await;
     assert_eq!(server_state, KenkuState::Online);
 }
